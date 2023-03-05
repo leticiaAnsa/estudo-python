c145ef6fca966ff4e4997fa026c44d2e1f1917c7
Ana Leticia <analeticia.ansa@gmail.com>
2023-03-05 17:14:53 +0000

    revisão de tuplas mundo 3

---
+++ aula 16.py
@@ -0,0 +1,54 @@
+#PRÁTICA
+#Tuplas são IMUTÁVEIS!!!
+#lanche[1] = 'Refrigerante' vai dar ERRO, pois tuplas são imutáveis
+
+#lanche = (tupla) [lista] {dicionário}
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida} ')
+
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}') 
+#print('Comi pra caramba')
+
+
+
+
+
+
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+#print(sorted(lanche))
+
+
+
+
+
+
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = b + a
+#print(a)
+#print(b)
+#print(len(c))
+#print(c)
+#print(c.index(5, 1))
+
+
+
+
+
+
+
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+del(pessoa)
+print(pessoa)
+
+
